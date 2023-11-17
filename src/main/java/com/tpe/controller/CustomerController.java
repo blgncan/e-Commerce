9f63f48af8180a1156d8ba620263a8cf4e2f7495
Betül Kuyucu <96betul@gmail.com>
2023-11-17 11:57:20 +0300

    getCustomer-Delete

--- src/main/java/com/tpe/controller/CustomerController.java
+++ src/main/java/com/tpe/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.tpe.controller;
 
 import com.tpe.domain.Customer;
+import com.tpe.dto.CustomerDTO;
 import com.tpe.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,7 +23,7 @@ public class CustomerController {
     //email daha önce kullanılmışsa hata fırlatır.(ConflictException)
 
     @PostMapping("/save")
-    public ResponseEntity<String> createCustomer(@Valid @RequestBody Customer customer){
+    public ResponseEntity<String> createCustomer(@Valid @RequestBody Customer customer) {
 
         customerService.saveCustomer(customer);
 
@@ -31,13 +32,31 @@ public class CustomerController {
 
     //14-a-Tüm customerları getirme -> http://localhost:8080/customers + GET
     @GetMapping
-    public ResponseEntity<List<Customer>> getAllCustomers(){
-        List<Customer> customerList=customerService.getAll();
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customerList = customerService.getAll();
         // return new ResponseEntity<>(customerList,HttpStatus.OK);//200
         return ResponseEntity.ok(customerList);//200
     }
 
+    //15-a-Id ile tek bir customer getirme -> http://localhost:8080/customers/1 + GET
+    //id tabloda yoksa hata fırlatır.(ResourceNotFoundException)
+    //15-b-CustomerDTO tanımlama
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerDTO> getCustomer(@PathVariable Long identity) {
 
+        CustomerDTO customerDTO = customerService.getCustomerDTO(identity);
+
+        return ResponseEntity.ok(customerDTO);//200
+    }
+
+    //16-a-id ile customer silme http://localhost:8080/customers/custom?id=1 + DELETE
+    @DeleteMapping("/custom")
+    public ResponseEntity<String> deleteCustomer(@RequestParam("id") Long id){
+
+        customerService.deleteCustomerById(id);
+
+        return ResponseEntity.ok("Customer is deleted successfully..");//200
+    }
 
 
 }
--- src/main/java/com/tpe/service/CustomerService.java
+++ src/main/java/com/tpe/service/CustomerService.java
@@ -1,7 +1,9 @@
 package com.tpe.service;
 
 import com.tpe.domain.Customer;
+import com.tpe.dto.CustomerDTO;
 import com.tpe.exception.ConflictException;
+import com.tpe.exception.ResourceNotFoundException;
 import com.tpe.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,10 +19,10 @@ public class CustomerService {
     //13-b-customer kaydetme
     public void saveCustomer(Customer customer) {
 
-        boolean isExists=customerRepository.existsByEmail(customer.getEmail());
-        if (isExists){
+        boolean isExists = customerRepository.existsByEmail(customer.getEmail());
+        if (isExists) {
             //13-d-custom exception fırlat
-            throw new ConflictException("Customer already exists by email:"+customer.getEmail());
+            throw new ConflictException("Customer already exists by email:" + customer.getEmail());
         }
 
         customerRepository.save(customer);
@@ -29,5 +31,29 @@ public class CustomerService {
     //14-b
     public List<Customer> getAll() {
         return customerRepository.findAll();//"FROM Customer"
+    }
+
+
+    //15-d-Id ile customer getirme
+    public Customer getCustomerById(Long id) {
+        Customer customer = customerRepository.findById(id).//SELECT * FROM Customer WHERE..
+                orElseThrow(() -> new ResourceNotFoundException("Customer is not found by id: " + id));
+        return customer;
+    }
+
+
+    //15-c-id ile Customer DTO getirme
+    public CustomerDTO getCustomerDTO(Long identity) {
+        Customer foundCustomer = getCustomerById(identity);
+
+        CustomerDTO customerDTO = new CustomerDTO(foundCustomer);  //customer-->customerDTO
+
+        return customerDTO;
+    }
+
+    //16-b
+    public void deleteCustomerById(Long id) {
+        getCustomerById(id);//customer yoksa custom exception fırlatılır.
+        customerRepository.deleteById(id);//DELETE FROM ...WHERE
     }
 }
